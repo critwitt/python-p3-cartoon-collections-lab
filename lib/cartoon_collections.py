88268f3c774179be88d818e43033019d5c4754bf
Chris Witte <christopher.t.witte@gmail.com>
2023-03-09 10:29:47 -0500

    solution

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,18 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for dwarf in dwarves:
+        print(f'{dwarves.index(dwarf) + 1}. {dwarf}')
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planateers):
+    return [f'{planateer.title()}!' for planateer in planateers]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(words):
+    for word in words:
+        if len(word) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(snacks):
+    for cheese in snacks:
+        if cheese == "cheddar" or cheese == "gouda" or cheese == "camembert":
+            return cheese
+    return None
